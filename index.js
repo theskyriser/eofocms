05c1b590aec0a71ebe77794c1720be58df4e9201
jacob-woodcock <jacob.woodcock6@gmail.com>
2022-09-25 22:50:10 -0500

    make it better

--- index.js
+++ index.js
@@ -31,11 +31,8 @@ app.use('/payroll', payrollRoutes)
 app.use('/financials', financialRoutes)
 app.use('/homework', homeworkRoutes)
 
-
-
-
 const CONNECTION_URL = "mongodb+srv://eOfe:valery01@eofedb.uqyzm.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || '8080'
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`)))
