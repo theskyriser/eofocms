bcb20702d7dbda13a7d5748d6263305d284dccfa
jacob-woodcock <jacob.woodcock6@gmail.com>
2022-09-25 22:17:04 -0500

    Added a Procfile.

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 
 
 const CONNECTION_URL = "mongodb+srv://eOfe:valery01@eofedb.uqyzm.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT
+const PORT = process.env.PORT | 3000
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`)))
