b14f756746fbad3e950ca4e226e12e09e7ec5efa
jacob-woodcock <jacob.woodcock6@gmail.com>
2022-09-25 22:42:57 -0500

    make it better

--- index.js
+++ index.js
@@ -35,10 +35,14 @@ app.get('/', (req, res) => {
     res.send('APP IS RUNNING')
 })
 
+app.get('/favico.ico', (req, res) => {
+    res.send('/favico.ico')
+})
+
 
 
 const CONNECTION_URL = "mongodb+srv://eOfe:valery01@eofedb.uqyzm.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`)))
